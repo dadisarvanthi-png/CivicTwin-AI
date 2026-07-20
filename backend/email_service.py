4c887810fb149645a7c1c1459e6e0ec86823710b
dadisarvanthi-png <dadisarvanthi@gmail.com>
2026-07-20 21:33:11 +0530

    Fix email service

--- backend/email_service.py
+++ backend/email_service.py
@@ -1,9 +1,9 @@
 from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
 
 conf = ConnectionConfig(
-    MAIL_USERNAME="dadisarvanthi@gmail.com",
-    MAIL_PASSWORD="nxvscxmvnjbvglrc",
-    MAIL_FROM="dadisarvanthi@gmail.com",
+    MAIL_USERNAME=dadisarvanthi@gmail.com
+    MAIL_PASSWORD=nxvscxmvnjbvglrc
+    MAIL_FROM=dadisarvanthi@gmail.com
     MAIL_PORT=587,
     MAIL_SERVER="smtp.gmail.com",
     MAIL_STARTTLS=True,
@@ -12,11 +12,11 @@ conf = ConnectionConfig(
 )
 
 async def send_status_email(email: str, status: str):
-    try:
-        message = MessageSchema(
-            subject="CivicTwin AI - Complaint Status Updated",
-            recipients=[email],
-            body=f"""
+
+    message = MessageSchema(
+        subject="Complaint Status Updated",
+        recipients=[email],
+        body=f"""
 Hello,
 
 Your complaint status has been updated.
@@ -24,16 +24,11 @@ Your complaint status has been updated.
 Current Status:
 {status}
 
-Thank you for using CivicTwin AI.
+Thank you.
 """,
-            subtype="plain",
-        )
+        subtype="plain",
+    )
 
-        fm = FastMail(conf)
-        await fm.send_message(message)
+    fm = FastMail(conf)
 
-        print("✅ Email sent successfully")
-
-    except Exception as e:
-        print("❌ Email sending failed")
-        print(e)
+    await fm.send_message(message)
